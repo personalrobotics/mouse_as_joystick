eddf2291fc601273a6ec1e5ee2615da25759c85c
Shervin Javdani <sjavdani@cmu.edu>
2016-07-24 07:33:54 -0700

    initial commit

---
+++ src/mouse_as_joystick/MouseToRosMessage.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+#reads specified joystick values, and publishes them
+
+import struct
+
+max_joystick_dx = 20.
+max_joystick_dy = 20.
+
+
+#make sure to run the following to be able to read:
+# sudo chmod a+r /dev/input/mouse1
+#to disable, first run the following:
+# xinput
+#find the device id of the mouse you want to disable. Then
+# xinput set-prop id "Device Enabled" 0
+mouse_filename = "/dev/input/mouse1"
+
+class MouseHandler(object):
+    def __init__(self):
+       self.mouse_file = open(mouse_filename, "rb", )
+   
+
+def getMouseEvent(mouse_file):
+    buf = mouse_file.read(3);
+    button = ord( buf[0] );
+    bLeft = button & 0x1;
+    bMiddle = ( button & 0x4 ) > 0;
+    bRight = ( button & 0x2 ) > 0;
+    x,y = struct.unpack( "bb", buf[1:] );
+    print ("L:%d, M: %d, R: %d, x: %d, y: %d\n" % (bLeft,bMiddle,bRight, x, y) );
+
+
+if __name__ == "__main__":
+    print 'asdf'
+    mouse_handler = MouseHandler()
+
+    while( 1 ):
+        getMouseEvent(mouse_handler.mouse_file);
+    mouse_handler.mouse_file.close();
+
